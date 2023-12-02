6a8468f64327ce156c1bc0c3de93377377ed04a0
paikwiki <paikwiki@gmail.com>
2023-12-02 17:11:04 +0900

    :sparkles: Done day01

---
+++ day01/trebuchet.py
@@ -0,0 +1,67 @@
+def solve1(lines: str):
+    sum = 0
+    for line in lines.strip().splitlines():
+        digits = [chr for chr in line if chr.isdigit()]
+        sum += int(digits[0] + digits[-1])
+
+    return sum
+
+
+def solve2(lines: str):
+    def parse(line: str):
+        SPELLED_DIGITS = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+        ]
+        first = last = ""
+        for i in range(len(line)):
+            if first != "":
+                break
+            if line[i].isdecimal():
+                first = line[i]
+            else:
+                for spelled in SPELLED_DIGITS:
+                    target = line[i : len(spelled) + i]
+                    if target == spelled:
+                        first = str(SPELLED_DIGITS.index(spelled) + 1)
+                        break
+
+        for i in range(len(line)):
+            if last != "":
+                break
+            reverse_i = len(line) - 1 - i
+            if line[reverse_i].isdecimal():
+                last = line[reverse_i]
+            else:
+                for spelled in SPELLED_DIGITS:
+                    target = line[reverse_i - len(spelled) + 1 : reverse_i + 1]
+                    if target == spelled:
+                        last = str(SPELLED_DIGITS.index(spelled) + 1)
+                        break
+
+        return first + last
+
+    sum = 0
+    for line in lines.strip().splitlines():
+        sum += int(parse(line))
+
+    return sum
+
+
+def test(lines):
+    result = solve1(lines)
+    print(f"result: {result}")
+    result = solve2(lines)
+    print(f"result: {result}")
+
+
+if __name__ == "__main__":
+    with open("input") as input:
+        test(input.read())
